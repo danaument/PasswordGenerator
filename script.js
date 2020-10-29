c8f574f8aff48d6cd6ad66c1a8dd081a56b72b89
Dan Aument <danaument@gmail.com>
2020-10-29 12:46:04 -0500

    generator working but with bad formatting, error checking started but needs work

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");  //drill down to button element with tag generate
-var sourceUpper = "abcdefghijklmnopqrstuvwxyz";
-var sourceLower = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var sourceNum = "0123456789";
-var sourceSpecial = ' !"#$%&\'()*+,-./:;<=>?@[\\\]^_`{|}~\ '; //needs work - how to actually include "\"?
-sourceUpper.split("");
-sourceLower.split("");
-sourceNum.split("");
-sourceSpecial.split("");
+var sourceUpper = "abcdefghijklmnopqrstuvwxyz".split("");
+var sourceLower = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var sourceNum = "0123456789".split("");
+var sourceSpecial = " !#$%&\'()*+,-./:;<=>?@[\\\]^_`{|}~\ ".split(""); //needs work - how to actually include "\"?
+
+function checkPassword(array) {
+  array.filter(function(n) {
+    return password.indexOf(n) !== -1;
+    })
+};
+
 
 // Write password to the #password input
 function writePassword() {
@@ -41,6 +44,7 @@ function writePassword() {
       var arrayBlank = [];
       var sourceBank = [];
       var password = [];
+      var passwordIngredients = [];
       console.log(arrayBlank);
       console.log(sourceLower);
       console.log(sourceUpper);
@@ -48,7 +52,9 @@ function writePassword() {
       console.log(sourceSpecial);
       if (includeLower === true) {
         var sourceBank = arrayBlank.concat(sourceLower);
+        passwordIngredients = passwordIngredients.push(includeLower);  //add lowercase to error check array
         console.log("The password source bank is now: " + sourceBank);
+        console.log(passwordIngredients);
       }
       if (includeUpper === true) {
         var sourceBank = sourceBank.concat(sourceUpper);
@@ -63,9 +69,24 @@ function writePassword() {
         console.log("The password source bank is now: " + sourceBank);
       }
       
+      var password = [];
+      console.log("The initial password variable is: " + password);
+      //time to pick items from sourceBank to append to password
 
+      do {
+        var password = [];
+        for (i = 0; i < passwordLength; i++) {
+          //pick random 
+          password.push(sourceBank[Math.floor(Math.random() * sourceBank.length)]);
+          console.log(password);
+        }
+      } 
+      while (passwordIngredients.foreach(checkPassword) < passwordIngredients.length );  //need to run checkPassword against each array in passwordIngredients array
+      
+      
+      toString(password);
 
-      // passwordText.value = password;  //go to tag with password id; write password variable contents to value: "The value property sets or returns the value of the value attribute of a text field."
+      passwordText.value = password;  //go to tag with password id; write password variable contents to value: "The value property sets or returns the value of the value attribute of a text field."
     }
   }
 
@@ -77,3 +98,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);  //create clickable button at button, run writePassword function
+
