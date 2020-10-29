a6cbc3c2cfab8686672a66f6af267b19a4e50ead
Dan Aument <danaument@gmail.com>
2020-10-29 14:54:22 -0500

    help from leah, reorganized some things. still need to get join method to work and handle password array correctly

--- script.js
+++ script.js
@@ -5,19 +5,28 @@ var sourceLower = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var sourceNum = "0123456789".split("");
 var sourceSpecial = " !#$%&\'()*+,-./:;<=>?@[\\\]^_`{|}~\ ".split(""); //needs work - how to actually include "\"?
 
+
 function checkPassword(array) {
+  console.log(workingPassword);
   array.filter(function(n) {
-    return password.indexOf(n) !== -1;
+    return workingPassword.indexOf(n) !== -1;
     })
+  if (array === null) {
+    tryAgain = false;
+  }
 };
 
+// function checkPassword(array) {
+//   array.filter(value => workingPassword.includes(value))
+// }
 
-// Write password to the #password input
+// Write workingPassword to the #workingPassword input
 function writePassword() {
-  // var password = generatePassword();
-  var passwordText = document.querySelector("#password");  //drill down to password id
+  
+  // var workingPassword = generatePassword();
+  var passwordText = document.querySelector("#workingPassword");  //drill down to workingPassword id
 
-  var passwordLength = Number(prompt("Please enter the desired password length, from 8 to 256."));
+  var passwordLength = Number(prompt("Please enter the desired workingPassword length, from 8 to 256."));
   console.log(passwordLength);
   console.log(Number.isInteger(passwordLength));
   if (Number.isInteger(passwordLength) !== true || passwordLength < 8 || passwordLength > 256) {
@@ -25,68 +34,105 @@ function writePassword() {
   }
   else {
   
-    var includeLower = confirm("Should the password include lowercase letters?  (Click 'Okay' to include or 'Cancel' to not include.)");
+    var includeLower = confirm("Should the workingPassword include lowercase letters?  (Click 'Okay' to include or 'Cancel' to not include.)");
     console.log("Lowercase letters included: " + includeLower);
-    var includeUpper = confirm("Should the password include uppercase letters?  (Click 'Okay' to include or 'Cancel' to not include.)");
+    var includeUpper = confirm("Should the workingPassword include uppercase letters?  (Click 'Okay' to include or 'Cancel' to not include.)");
     console.log("Uppercase letters included: " + includeUpper);
-    var includeNum = confirm("Should the password include numbers?  (Click 'Okay' to include or 'Cancel' to not include.)");
+    var includeNum = confirm("Should the workingPassword include numbers?  (Click 'Okay' to include or 'Cancel' to not include.)");
     console.log("Numbers included: " + includeNum);
-    var includeSpecial = confirm("Should the password include special characters?  (Click 'Okay' to include or 'Cancel' to not include.)");
+    var includeSpecial = confirm("Should the workingPassword include special characters?  (Click 'Okay' to include or 'Cancel' to not include.)");
     console.log("Symbols included: " + includeSpecial);
     console.log(includeLower + includeUpper + includeNum + includeSpecial);
-        
+    
+    var passwordIngredients = {
+      lower: includeLower,
+      upper: includeUpper,
+      num: includeNum,
+      special: includeSpecial,
+    };
+    
+
     if (includeLower + includeUpper + includeNum + includeSpecial === 0 ) {
       alert("You must include at least one character type.");
     }
 
     else {
-      // time to create source of characters for password
+      // time to create source of characters for workingPassword
       var arrayBlank = [];
       var sourceBank = [];
-      var password = [];
-      var passwordIngredients = [];
+      var workingPassword = [];
+      
       console.log(arrayBlank);
       console.log(sourceLower);
       console.log(sourceUpper);
       console.log(sourceNum);
       console.log(sourceSpecial);
       if (includeLower === true) {
-        var sourceBank = arrayBlank.concat(sourceLower);
-        passwordIngredients = passwordIngredients.push(includeLower);  //add lowercase to error check array
-        console.log("The password source bank is now: " + sourceBank);
+        sourceBank = arrayBlank.concat(sourceLower);
+        // passwordIngredients = passwordIngredients.push(includeLower);  //add lowercase to error check array, syntax?
+        console.log("The workingPassword source bank is now: " + sourceBank);
         console.log(passwordIngredients);
       }
       if (includeUpper === true) {
-        var sourceBank = sourceBank.concat(sourceUpper);
-        console.log("The password source bank is now: " + sourceBank);
+        sourceBank = sourceBank.concat(sourceUpper);
+        console.log("The workingPassword source bank is now: " + sourceBank);
       }
       if (includeNum === true) {
-        var sourceBank = sourceBank.concat(sourceNum);
-        console.log("The password source bank is now: " + sourceBank);
+        sourceBank = sourceBank.concat(sourceNum);
+        console.log("The workingPassword source bank is now: " + sourceBank);
       }
       if (includeSpecial === true) {
-        var sourceBank = sourceBank.concat(sourceSpecial);
-        console.log("The password source bank is now: " + sourceBank);
+        sourceBank = sourceBank.concat(sourceSpecial);
+        console.log("The workingPassword source bank is now: " + sourceBank);
       }
-      
-      var password = [];
-      console.log("The initial password variable is: " + password);
-      //time to pick items from sourceBank to append to password
+      var tryAgain = true;
+      workingPassword = [];
+      console.log("The initial workingPassword variable is: " + workingPassword);
+      //time to pick items from sourceBank to append to workingPassword
 
-      do {
-        var password = [];
-        for (i = 0; i < passwordLength; i++) {
+      while (tryAgain === true) {
+          for (i = 0; i < passwordLength; i++) {
           //pick random 
-          password.push(sourceBank[Math.floor(Math.random() * sourceBank.length)]);
-          console.log(password);
-        }
-      } 
-      while (passwordIngredients.foreach(checkPassword) < passwordIngredients.length );  //need to run checkPassword against each array in passwordIngredients array
-      
-      
-      toString(password);
+            workingPassword.push(sourceBank[Math.floor(Math.random() * sourceBank.length)]);
+            console.log(workingPassword);
+          }
+          if (passwordIngredients.lower) {
+            checkPassword(sourceLower);
+          }
 
-      passwordText.value = password;  //go to tag with password id; write password variable contents to value: "The value property sets or returns the value of the value attribute of a text field."
+          if (passwordIngredients.upper) {
+            checkPassword(sourceUpper);
+          }
+
+          if (passwordIngredients.num) {
+            checkPassword(sourceNum);
+          }
+          
+          if (passwordIngredients.special) {
+            checkPassword(sourceSpecial);
+          }
+           
+          // for (i = 0; i < passwordLength; i++) {
+          //           //pick random 
+          //   workingPassword.push(sourceBank[Math.floor(Math.random() * sourceBank.length)]);
+          //   console.log(workingPassword);
+          //   }
+          // console.log(passwordIngredients.forEach(checkPassword()));
+          // console.log(checkPassword(passwordIngredients.forEach));
+
+
+
+      }
+        
+
+      // } 
+      // while (passwordIngredients.foreach(checkPassword) < passwordIngredients.length );  //need to run checkPassword against each array in passwordIngredients array
+      console.log(Array.isArray(workingPassword));
+      workingPassword.join();
+      console.log(Array.isArray(workingPassword));
+      workingPassword.toString();
+
+      passwordText.value = workingPassword;  //go to tag with workingPassword id; write workingPassword variable contents to value: "The value property sets or returns the value of the value attribute of a text field."
     }
   }
 
